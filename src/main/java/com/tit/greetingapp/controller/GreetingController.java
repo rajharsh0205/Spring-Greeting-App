bf05a35e759890a25071675ea72f2aad0604a80b
rajharsh0205 <rajharsh810201@gmail.com>
2025-02-25 13:36:47 +0530

    [Harsh] Add . Uc1 feature added

---
+++ src/main/java/com/tit/greetingapp/controller/GreetingController.java
@@ -0,0 +1,31 @@
+package com.tit.greetingapp.controller;
+
+import com.tit.greetingapp.DTO.Greeting;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/greeting")
+public class GreetingController {
+
+    @GetMapping
+    public Greeting getGreeting() {
+        return new Greeting("Hello, this is a GET request!");
+    }
+
+    @PostMapping
+    public Greeting postGreeting() {
+        return new Greeting("Hello, this is a POST request!");
+    }
+
+    @PutMapping
+    public Greeting putGreeting() {
+        return new Greeting("Hello, this is a PUT request!");
+    }
+
+    @DeleteMapping
+    public Greeting deleteGreeting() {
+        return new Greeting("Hello, this is a DELETE request!");
+    }
+
+
+}
