d743da62c0ae356254c1b9e4e8a7e0e9ba3f5860
rajharsh0205 <rajharsh810201@gmail.com>
2025-03-02 11:38:04 +0530

    [Harsh] Add . Uc5 feature added

--- src/main/java/com/tit/greetingapp/service/GreetingService.java
+++ src/main/java/com/tit/greetingapp/service/GreetingService.java
@@ -1,10 +1,17 @@
 package com.tit.greetingapp.service;
 
+import com.tit.greetingapp.model.Greeting;
+import com.tit.greetingapp.repository.GreetingRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class GreetingService {
 
+    private final GreetingRepository greetingRepository;
+
+    public GreetingService(GreetingRepository greetingRepository) {
+        this.greetingRepository = greetingRepository;
+    }
     public String getGreetingMessage() {
         return "Hello World";
     }
@@ -18,5 +25,16 @@ public class GreetingService {
         } else if (lastName != null) {
             return "Hello, " + lastName + "!";
         } else return getGreetingMessage();  // Reusing UC2 method for default
+    }
+
+    public Greeting saveGreeting(String message) {
+        Greeting greeting = new Greeting(message); // Create a Greeting entity
+        return greetingRepository.save(greeting);  // Save to database
+    }
+
+    // UC5 - Retrieve a Greeting by ID
+    public Greeting getGreetingById(Long id) {
+        return greetingRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Greeting not found for ID: " + id));
     }
 }
